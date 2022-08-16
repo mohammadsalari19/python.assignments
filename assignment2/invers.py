7182ee2c17e9c09640546c28a79b0eb1267804f6
mohammadjafar salari <110528959+mohammadsalari19@users.noreply.github.com>
2022-08-16 14:49:35 +0430

    Delete invers.py

--- assignment2/invers.py
+++
@@ -1,8 +0,0 @@
-number=int(input('enter an integer'))
-
-if(number==0):
-    print('the invers of zero is ambiguous')
-elif(number==1 or number==-1):
-    print('the number is equal its invers')
-else:
-     print('the number isnot equal its invers')
