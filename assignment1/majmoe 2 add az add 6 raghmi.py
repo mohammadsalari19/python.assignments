764e341309fa663f42e9b1f2ea4dc5555644a911
mohammadjafar salari <110528959+mohammadsalari19@users.noreply.github.com>
2022-08-25 20:00:01 +0430

    Add files via upload

---
+++ assignment1/majmoe 2 add az add 6 raghmi.py
@@ -0,0 +1,5 @@
+number=int(input('add 6 raghmi ra vared konid:'))
+add2=int((number%100)/10)
+add5=int((number%100000)/10000)
+majmoe=add2+add5
+print('majmoe add dovom v panjome barabar ast ba:',majmoe)
