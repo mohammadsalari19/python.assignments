7b4c83df59d5c97bd879fbd2dd4db3423bbfb8de
mohammadjafar salari <110528959+mohammadsalari19@users.noreply.github.com>
2022-08-28 01:35:56 +0430

    Update ATM.py

--- assignment3/ATM.py
+++ assignment3/ATM.py
@@ -1,27 +1,28 @@
 ramz=[1,2,3,4]
 khtar=[4,3,2,1]
-ramz_karbar=[]
+
 counter=0
 while counter<3:
     
     ramz_karbar1=int(input('ramz ra vared konid : '))
-    
+    ramz_karbar=[]  
+     
     if ramz_karbar1//10000==0 and ramz_karbar1//1000!=0:
         
         for j in range(4):
             
-            add=ramz_karbar1%10
-            ramz_karbar.append(add)
-            ramz_karbar1=ramz_karbar1//10
-    
+          add=ramz_karbar1%10
+          ramz_karbar.append(add)
+          ramz_karbar1=ramz_karbar1//10
+        
         if ramz_karbar==khtar:
             
-            print('vared shavid') 
+            print('khos omadid ghasd anjam che amalyati darid ') 
             break
             
         elif  ramz_karbar==ramz:
             
-            print('lotfan montazer bemanid dastgah dar hal apdyt ast')#zang polis
+            print('polic posht sarte dastato bzar ro saret dozd jn')
             break
         
         else:
@@ -30,15 +31,11 @@ while counter<3:
                print('hesab ghofl shod ') 
                
            else:
-                print('ramz ashteah ast dobare talash konid') 
-                counter+=1
+                print('ramz ashteah ast ') 
                 
                 
     else:
         print('tedad addad ramz eshtbah ast do bare talash konid')
         counter-=1
 
-            
-
-    
-    
+    counter+=1         
