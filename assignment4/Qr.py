bfca9179acadbe2411de4177dd82e9786e7a950a
mohammadjafar salari <110528959+mohammadsalari19@users.noreply.github.com>
2022-09-01 21:20:26 +0430

    Update Qr.py

--- assignment4/Qr.py
+++ assignment4/Qr.py
@@ -3,7 +3,7 @@ import qrcode
 
 qr_matn = input('matn ra vared konid : ')
 
-qr_address = input('address ra vared konid')
+qr_address = input('address ra vared konid : ')
 
 Qr =qrcode.make(qr_matn)
 Qr.save(qr_address)
